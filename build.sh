721d5b9a4c32341d64edffe024f2325b678dfdda
Tomas Bjerre <tomas.bjerre85@gmail.com>
2016-03-04 21:02:25 +0100

    Logging when building with build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mvn clean eclipse:eclipse install || exit 1
 cd violation-comments-to-github-maven-plugin-example
-mvn violation-comments-to-github-maven-plugin:violation-comments -DGITHUB_PULLREQUESTID=false -DGITHUB_OAUTH2TOKEN=$GITHUB_OAUTH2TOKEN -e
-mvn violation-comments-to-github-maven-plugin:violation-comments -DGITHUB_PULLREQUESTID=$TRAVIS_PULL_REQUEST -DGITHUB_OAUTH2TOKEN=$GITHUB_OAUTH2TOKEN -e
+mvn violation-comments-to-github-maven-plugin:violation-comments -DGITHUB_PULLREQUESTID=false -DGITHUB_OAUTH2TOKEN=$GITHUB_OAUTH2TOKEN -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -e
+mvn violation-comments-to-github-maven-plugin:violation-comments -DGITHUB_PULLREQUESTID=$TRAVIS_PULL_REQUEST -DGITHUB_OAUTH2TOKEN=$GITHUB_OAUTH2TOKEN -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -e
